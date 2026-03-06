271fe7873a919f3e6c7a94dc438dd4fc79461ca9
Jessie Montaro <montaro.daedra@gmail.com>
2026-03-06 04:00:39 +0100

    Fixed some type annotations

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -9,7 +9,7 @@ class CarWashStation:
     def __init__(
         self, distance_from_city_center: float, clean_power: int,
         average_rating: float, count_of_ratings: int
-    ):
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -21,17 +21,17 @@ class CarWashStation:
         wash_price = (service * ws_pricing)
         return round(wash_price, 1)
 
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rating):
+    def rate_service(self, rating: int) -> None:
         total_rating = self.average_rating * self.count_of_ratings
         total_rating += rating
         self.count_of_ratings += 1
         self.average_rating = round((total_rating / self.count_of_ratings), 1)
 
-    def serve_cars(self, cars: list):
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
